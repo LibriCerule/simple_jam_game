e5dd3ff36395ced561f7d0cb9f38ca1b54cb4fbc
Declan Whitford Jones <wjdeclan@gmail.com>
2016-06-01 17:00:18 -0400

    Mouse recoil from shots

--- simple_jam_game/core/src/simplejam/game/MouseFollowStrategy.java
+++ simple_jam_game/core/src/simplejam/game/MouseFollowStrategy.java
@@ -12,6 +12,8 @@ public class MouseFollowStrategy extends Strategy implements InputProcessor {
     //Store last checked mouse position
     float mouseX = 0;
     float mouseY = 0;
+    Vector2 kick = new Vector2(0,0);
+    Vector2 pos = new Vector2(0,0);
 
     //Unused
     @Override
@@ -34,6 +36,7 @@ public class MouseFollowStrategy extends Strategy implements InputProcessor {
     //Unused
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        kick = new Vector2((pos.x - screenX) * -1, ((Gdx.graphics.getHeight() - pos.y) - screenY)).nor().scl(15f);
         return false;
     }
 
@@ -66,7 +69,9 @@ public class MouseFollowStrategy extends Strategy implements InputProcessor {
     public Vector2 getMovement(Sprite entity, float delta) {
         //Calculate movement vector, half the distance to the cursor every second
         //Adjusting for 0,0 being top left instead of bottom left with mouse input
-        Vector2 movement = new Vector2((entity.getX() - mouseX + entity.getWidth()/2) * -1.5f * delta,(mouseY - (Gdx.graphics.getHeight() - entity.getY()) + + entity.getHeight()/2) * -1.5f * delta);
+        Vector2 movement = new Vector2((entity.getX() - mouseX + entity.getWidth()/2) * -1.5f * delta - kick.x,(mouseY - (Gdx.graphics.getHeight() - entity.getY()) + + entity.getHeight()/2) * -1.5f * delta - kick.y);
+        kick = new Vector2(0,0);
+        pos = new Vector2(entity.getX() + entity.getWidth()/2, entity.getY() + entity.getHeight()/2);
         return movement;
     }
 }
