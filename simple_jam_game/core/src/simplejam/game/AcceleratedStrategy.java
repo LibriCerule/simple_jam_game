77605a1b5336870462b3e535dcfc6c4039c55a50
Declan Whitford Jones <wjdeclan@gmail.com>
2016-05-31 23:26:41 -0400

    Basic AcceleratedStrategy

---
+++ simple_jam_game/core/src/simplejam/game/AcceleratedStrategy.java
@@ -0,0 +1,22 @@
+package simplejam.game;
+
+import com.badlogic.gdx.math.Vector2;
+
+/**
+ * Created by Declan on 5/31/2016.
+ */
+public class AcceleratedStrategy extends Strategy {
+    Vector2 velocity;
+    Vector2 acceleration;
+
+    public AcceleratedStrategy(Vector2 velocity, Vector2 acceleration) {
+        this.acceleration = acceleration;
+        this.velocity = velocity;
+    }
+
+    @Override
+    public Vector2 getMovement(float entityX, float entityY, float delta) {
+        velocity = velocity.add(acceleration.scl(delta));
+        return velocity;
+    }
+}
